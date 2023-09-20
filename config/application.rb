f9bf39d7a25eef514a2ed2c0691979f495874a47
zeeker <zeekerpro@gmail.com>
2023-09-20 22:13:00 +0800

    update rails6 to rails7.1.beta1

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module LaobiaoBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
     config.autoload_lib(ignore: %w(assets tasks))
